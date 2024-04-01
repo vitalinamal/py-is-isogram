68645c073353369c890bc3f41fb66456fbb8115c
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 22:42:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestCoinCombination:
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True)
+        ]
+    )
+    def test_get_coin_combination(self,
+                                  word: str,
+                                  expected_result: bool) -> None:
+        assert is_isogram(word) is expected_result
